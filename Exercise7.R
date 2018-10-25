6d51f6f0d5825cb5815b9e061fc3fb20f6bb68d3
nhermann1 <nhermann@nd.edu>
2018-10-24 23:14:39 -0400

    final annotated script

--- Exercise7.R
+++ Exercise7.R
@@ -1,38 +1,33 @@
 setwd("~/Documents/Biocomp/Biocomp-Fall2018-181012-Exercise7/")
 rm(list=ls())  #clear variables
 
-library(dplyr)
-
 #Odd rows of data frame function, pass data frame as argument
-dataOdd<-function(a) {
-  oddData<-a[seq(1,nrow(a),by=2),]
+dataOdd<-function(a) { #The data frame is the only argument passed
+  oddData<-a[seq(1,nrow(a),by=2),] #create sequence of odd numbers and return those rows
   return(oddData)
 }
 iris<-read.csv("iris.csv")
-dataOdd(iris)
+dataOdd(iris) #iris is the data frame in this case
 
 #Count observations of given species, pass data frame, species
-speciesCount<-function(d,s) {
-  nrow(d[d$Species=="s",]) #Can't make this work
+speciesCount<-function(d,s) { #d is the data frame, s is the species
+  nrow(d[d$Species==s,]) #subsets the data to the passed species and counts the rows in that subset
 }
-speciesCount(iris,setosa)
+speciesCount(iris,"setosa") #iris is data frame, setosa is species--must be in quotes to be a string
 
 #Subset data frame greater than given sepal width, pass width to be greater
-largeSepals<-function(d,w) {
-  great<-d[d$Sepal.Width>w,]
-  data.frame(great)
+largeSepals<-function(d,w) { #d is data frame, w is the numeric width
+  great<-d[d$Sepal.Width>w,] #subsets the data to what's greater than passed width
+  largeSepals<<-data.frame(great) #the subset is then saved in the global environment
 }
-largeSepals<-largeSepals(iris,3.0) #Is this okay to save it outside the function
+largeSepals(iris,3.2) #iris is the data frame, 3.2 is the numeric width
 
 #Write csv of species subset
-speciesCSV<-function(d) {
-  s<-d[d$Species=="setosa",]
-  name<-paste0(s$Species,".csv")
-  write.csv(s,file=)
+speciesCSV<-function(d,s) { #d is the data frame, s is the species
+  oneSpecies<-d[d$Species==s,] #subsets the data to the passed species
+  return(write.csv(oneSpecies,file=paste0(s,".csv"))) #write out that subset pasting the .csv to the filename, use paste0 to keep out a space between the strings
 }
-speciesCSV(iris)
-
-
+speciesCSV(iris,"setosa") #iris is the data frame, setosa is the species--must be in quotes to be a string
 
 
 
